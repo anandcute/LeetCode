445627095064fe71b9acd5b2fb26818db67bf299
anandcute <d.anandkumar2002@gmail.com>
2024-10-03 13:08:01 +0530

    commit

---
+++ Easy/MaximumSumSubarray.java
@@ -0,0 +1,20 @@
+public class MaximumSumSubarray {
+    public static int maxSubArray(int[] nums) {
+        int maxsum = nums[0];
+        int currentsum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if(currentsum < 0){
+                currentsum = nums[i];
+            }
+            else{
+                currentsum += nums[i];
+            }
+        }
+        maxsum = Math.max(maxsum, currentsum);
+        return maxsum;
+    }
+    public static void main(String[] args) {
+        int nums[] = {-1,2,-1,3,-1,-4,3,-1,7,-1};
+        System.out.println(maxSubArray(nums));
+    }
+}
